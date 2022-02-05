876d8ee7835d5a5be37301fffee25aee4775e5a3
Ridwan Taiwo <donriddo@gmail.com>
2022-02-05 04:39:07 +0100

    refactor: rework user model to using sqlalchemy queries

--- src/users/user_model.py
+++ src/users/user_model.py
@@ -1,7 +1,36 @@
+from sqlalchemy.dialects.postgresql import UUID
 from setup import db
+import uuid
+from sqlalchemy.sql import func
 
-class User(db.Document):
-  name = db.StringField()
-  email = db.StringField()
-  def to_json(self):
-    return {"name": self.name,"email": self.email}
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    uuid = db.Column(UUID(as_uuid=True), default=uuid.uuid4)
+    first_name = db.Column(db.String())
+    last_name = db.Column(db.String())
+    email = db.Column(db.String(), unique=True)
+    created_at = db.Column(db.DateTime(
+        timezone=True), server_default=func.now())
+    updated_at = db.Column(db.DateTime(timezone=True), onupdate=func.now())
+
+    def __init__(self, email, first_name, last_name):
+        self.email = email
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def to_json(self):
+        return {
+            "id": self.id,
+            "email": self.email,
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "uuid": self.uuid,
+            "created_at": self.created_at,
+            "updated_at": self.updated_at
+        }
+
+    def __repr__(self):
+        return f"{self.first_name}:{self.last_name}"
