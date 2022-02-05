15f535912463a2f09ca74c38ea6c77fe15fe326a
Ridwan Taiwo <donriddo@gmail.com>
2022-02-05 04:40:20 +0100

    refactor: add pagination to list users

--- src/users/user_controller.py
+++ src/users/user_controller.py
@@ -2,48 +2,75 @@
 # encoding: utf-8
 import json
 from flask import Blueprint, request, jsonify, make_response
-from src.users.user_repo import create_user, fetch_all_users
+from src.users.user_repo import create_user, fetch_all_users, fetch_user
 
 user_api = Blueprint('user', __name__)
 
+
 @user_api.route('/', methods=['POST'])
 def create_new_user():
-  info = json.loads(request.data)
-  email = info.get('email', '')
-  name = info.get('name', '')
-  user = create_user({"email":email,"name":name})
-  if user:
-      return make_response(jsonify({"data":user}), 200)
-  else:
-      return make_response(
-        jsonify({"status": 400, "message": "error creating user"}),
-        400
-      )
+    info = json.loads(request.data)
+    email = info.get('email', '')
+    first_name = info.get('first_name', '')
+    last_name = info.get('last_name', '')
+    user = create_user(
+        {"email": email, "first_name": first_name, "last_name": last_name})
+    if user:
+        return make_response(
+            jsonify({
+                "status": 200,
+                "message": "created successfully",
+                "data": user
+            }),
+            200
+        )
+    else:
+        return make_response(
+            jsonify({"status": 400, "message": "error creating user"}),
+            400
+        )
+
 
 @user_api.route('/', methods=['GET'])
 def list_users():
-  users = fetch_all_users()
-  return make_response(
-        jsonify({
-          "status": 200,
-          "message": "users retrieved successfully",
-          "data": users
-        }),
+    page = 1
+    per_page = 10
+    try:
+        page = int(request.args.get('page'))
+        per_page = int(request.args.get('per_page'))
+    except:
+        pass
+
+    result = fetch_all_users(page=page, per_page=per_page)
+    print(result)
+    return make_response(
+        {
+            "status": 200,
+            "message": "users retrieved successfully",
+            "data": result["data"],
+            "meta": result["meta"]
+        },
         200
-      )
+    )
 
 
 @user_api.route('/:id', methods=['GET'])
 def get_user_info():
-  uid = request.params.get('id')
-  name = request.args.get('name')
-  email = request.args.get('email')
-  user = fetch_user({"email":email,"name":name})
-  if user:
-      return make_response(jsonify({"data":user}), 200)
-  else:
-      return make_response(
-        jsonify({"status": 404, "message": "user not found"}),
-        404
-      )
-
+    uid = request.params.get('id')
+    first_name = request.args.get('first_name')
+    email = request.args.get('email')
+    user = fetch_user({"email": email, "first_name": first_name})
+    if user:
+        return make_response(
+            jsonify({
+                "status": 200,
+                "message": "user retrieved successfully",
+                "data": user
+            }),
+            200
+        )
+    else:
+        return make_response(
+            jsonify({"status": 404, "message": "user not found"}),
+            404
+        )
