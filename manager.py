eb1e4ddd682c88663cf6d313ceb91602d9078398
Ridwan Taiwo <donriddo@gmail.com>
2022-02-06 01:15:23 +0100

    refactor: startup logic

--- manager.py
+++ manager.py
@@ -1,15 +1,20 @@
 # coding: utf-8
+import os
 from flask_script import Manager
 from src import create_app
 # Used by app debug & livereload
 PORT = 7070
+if os.environ.get('PORT'):
+    PORT = os.environ.get('PORT')
 app = create_app()
 manager = Manager(app)
+
 
 @manager.command
 def run():
     """Run app."""
-    app.run(port=PORT)
+    app.run(host='0.0.0.0', port=PORT)
+
 
 if __name__ == "__main__":
     manager.run()
